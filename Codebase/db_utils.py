26c23ea6f68c9afe30fb67d433d01220b0625d03
Chenghan Sun <sunchenghan1995@gmail.com>
2020-03-16 11:07:21 -0700

    update sql method

--- Codebase/db_utils.py
+++ Codebase/db_utils.py
@@ -1,10 +1,65 @@
 import sys
+import csv
+import pandas as pd
 sys.path.insert(0, '/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages')
 import pymysql
-import csv
 
 
 class YelpDb:
+    """ Yelp Database Configurations and database utilities
+    """
+
+    def __init__(self, db_name):
+        """ db connection
+        """
+        self.host = "localhost"
+        self.user = "root"
+        self.password = "skrskr220"
+        self.db = db_name
+        self.charset = 'utf8'
+        self.conn = pymysql.connect(host=self.host,
+                                    user=self.user,
+                                    password=self.password,
+                                    db=self.db,
+                                    charset=self.charset)
+        self.cursor = self.conn.cursor()  # set a cursor object
+        
+    def execute(self, sql):
+        """ sql execution
+        """
+        try:
+            self.cursor.execute(sql)
+            self.conn.commit()
+        except pymysql.Error:
+            self.conn.rollback()
+            print(sql)
+
+    def fetch_all(self):
+        """ fetch all documents
+        """
+        return self.cursor.fetchall()
+
+    def fetch_one(self):
+        """ fetch single document
+        """
+        return self.cursor.fetchone()
+
+    def db_to_df(self, table_name):
+        """ read dbs into Pandas DataFrame
+        """
+        yelp_df = pd.read_sql(f"SELECT * FROM {table_name};", self.conn)
+        return yelp_df
+    
+    def close(self):
+        """ disconnect from server
+        """
+        self.cursor.close()
+        self.conn.close()
+        
+
+
+# keep this class temperary
+class YelpDb_backup:
     """ Yelp Database Configurations
     """
 
@@ -77,10 +132,3 @@ class YelpDb:
         self.conn.close()  # disconnect from server
 
 
-# TEST
-# yelp_db_test = YelpDb()
-# yelp_db_test.create_yelp_table('YELP_SF')
-# csv_path = '../Database/Yelp_all.csv'
-# yelp_db_test.csv_to_db(csv_path, 'YELP_SF')
-# yelp_db_test.close()
-print("done")
